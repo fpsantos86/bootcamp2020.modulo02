516cc1d953dcc49fd04f8ffc9eb3eb509131d8ca
unknown <fpsantos86@hotmail.com>
2020-02-23 22:15:28 -0300

    Model de usuario / Criando loader de models

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello Rocketseat' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Felipe Pereira dos Santos',
+    email: 'fpsantos86@hotmail.com',
+    password_hash: '1238712387',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
